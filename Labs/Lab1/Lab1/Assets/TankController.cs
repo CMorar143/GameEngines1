a363b9443104381926e558048d2fdc28c10b1e68
CMorar143 <cian@morar.org>
2019-09-27 11:52:04 +0100

    reinitialising files

---
+++ Labs/Lab1/Lab1/Assets/TankController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    private TankController Tank;
+    public float speed = 5f;
+    public float rotspeed = 45f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tank = GetComponent<TankController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tank.transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        Tank.transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotspeed, 0f);
+    }
+}
