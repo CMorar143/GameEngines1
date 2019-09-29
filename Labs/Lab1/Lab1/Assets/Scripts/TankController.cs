4a64e5b45f9ed6f074f3fb7e0c37e6212a99b9b0
CMorar143 <cian@morar.org>
2019-09-29 14:10:55 +0100

    added the shooting

--- Labs/Lab1/Lab1/Assets/Scripts/TankController.cs
+++ Labs/Lab1/Lab1/Assets/Scripts/TankController.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
-    private TankController Tank;
     public float speed = 5f;
     public float rotspeed = 65f;
+
+    public GameObject bulletSpawnPoint;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        Tank = GetComponent<TankController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Tank.transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
-        Tank.transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotspeed, 0f);
+        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotspeed, 0f);
+
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab);
+            bullet.transform.position = bulletSpawnPoint.transform.position;
+            bullet.transform.rotation = transform.rotation;
+        }
     }
 }
