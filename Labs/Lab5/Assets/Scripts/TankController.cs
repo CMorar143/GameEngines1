a69db1002f073d96b818a902703559c35ca374b7
CMorar143 <cian@morar.org>
2019-11-02 18:26:19 +0000

    working on lab 5

---
+++ Labs/Lab5/Assets/Scripts/TankController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotspeed = 65f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotspeed, 0f);
+    }
+}
---
+++ Labs/Lab5/Assets/Scripts/TurretController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public float rotSpeed = 180.0f;
+    public float fireRate = 5.0f;
+    public Transform bulletSpawnPoint;
+    public Transform Tank;
+    public GameObject bulletPrefab;
+    private bool hasCoroutineStarted = false;
+    private float timeCount = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            if (hasCoroutineStarted)
+            {
+                GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            }
+
+            yield return new WaitForSeconds(1.0f / fireRate);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Tank" && !hasCoroutineStarted)
+        {
+            hasCoroutineStarted = true;
+            StartCoroutine(Shoot());
+            Debug.Log("onTriggerEnter");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Tank")
+        {
+            hasCoroutineStarted = false;
+            Debug.Log("onTriggerExit");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Tank" && hasCoroutineStarted)
+        {
+            Debug.Log("onTriggerStay");
+
+            Vector3 toPlayer = other.transform.position - transform.position;
+
+            transform.rotation = Quaternion.RotateTowards
+                (
+                    transform.rotation,
+                    Quaternion.LookRotation(-toPlayer), 
+                    rotSpeed * Time.deltaTime
+                );
+        }
+    }
+}
