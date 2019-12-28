39a20551fc8531839ce3108e792c9c020a6f5d2a
CMorar143 <cian@morar.org>
2019-12-28 20:15:36 +0000

    added comments to wall alg

--- Labs/Lab2/Assets/Scripts/Wall.cs
+++ Labs/Lab2/Assets/Scripts/Wall.cs
@@ -17,13 +17,18 @@ public class Wall : MonoBehaviour
         {
             for (int j = -halfWidth; j < halfWidth; j++)
             {
+            	// Get position
                 Vector3 pos = transform.TransformPoint(new Vector3(j, 0.5f + i, 0));
 
+                // Create cube
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 cube.transform.position = pos;
 
+                // Change colour & add mass
                 cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.value, 1, 1);
                 cube.AddComponent<Rigidbody>().mass = 0.1f;
+
+                // Make the cube a child of the parent object
                 cube.transform.parent = this.transform;
             }
         }
